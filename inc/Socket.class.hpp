74b23bf9c0417a3cffec3cdacbd3d0afdaaa8bd9
Thomas Lafont <tlafont@2D4.42angouleme.fr>
2023-02-10 12:39:55 +0100

    started socket class

--- inc/Socket.class.hpp
+++ inc/Socket.class.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Socket.class.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlafont <tlafont@student.42angouleme.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 11:37:07 by tlafont           #+#    #+#             */
+/*   Updated: 2023/02/10 12:08:53 by tlafont          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 #ifndef SOCKET_HPP
 #define SOCKET_HPP
@@ -5,35 +17,54 @@
 // Includes
 
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 // Typedefs
 
 typedef unsigned short      uint16_t;
 typedef unsigned int        uint32_t;
 
+// class interface
+class ISocket
+{
+	public:
+	    //---- canonical form ----//
+			// constructors //
+				// default constructor
+		ISocket(int dom, int serv, int protoc, int port, uint32_t interf);
+			// destructor //
+	    virtual ~ISocket(void);
+			
+	    //---- getter methods ----//
+		int					getSocket() const;
+		int					getConnection() const;
+		struct sockaddr_in	getAddress() const;
 
-class Socket {
-public:
-    // Constructor and Destructor
-    Socket(void);
-    ~Socket(void);
+	    //---- member methods ----//
+				// virtual function to connect to a network
+				//(at define for a server or a client)
+		virtual int	connectToNetwork(int sock, struct sockaddr_in addr);
 
-    // methods
-    inline int		getFd(void) { return (this->_fd); }
+				// connection test establishment
+		void	testConnection(int to_test);
 
-//    int connect();
-//    int listen();
-//    int bind();
-//    int recv();
-//    int send();
-//    int setSockOpt();
-//    int getSockOpt();
+	//    int connect();
+	//    int listen();
+	//    int bind();
+	//    int recv();
+	//    int send();
+	//    int setSockOpt();
+	//    int getSockOpt();
 
-private:
-    Socket(const Socket& s);
-    Socket  &operator=(const Socket& s);
+	private:
+		int					_socket;
+		int					_connection;
+		struct sockaddr_in	_address;
 
-    int _fd;
+    	Socket(void);
+	    Socket(const Socket& s);
+	    Socket  &operator=(const Socket& s);
 };
 
 #endif
--- src/Socket.class.cpp
+++ src/Socket.class.cpp
@@ -1,9 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Socket.class.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlafont <tlafont@student.42angouleme.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 12:06:39 by tlafont           #+#    #+#             */
+/*   Updated: 2023/02/10 12:20:30 by tlafont          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/Socket.class.hpp"
 
 /*
-*  @brief	Default constructor of the class Socket.
+*  @brief	Default constructor of the class ISocket.
 *           Initialize the socket without any parameters
 *  @param	void
 *  @return	void
 */
-Socket::Socket(void): _fd(-1) {}
+ISocket::ISocket() {};
+
+/*
+*  @brief	Default constructor of the class ISocket.
+*           Initialize the socket with parameters
+*  @param	int , int , int , int , unsigned long
+*  @return	void
+*/
+ISocket::ISocket(int dom, int serv, int protoc, int port, uint32_t interf)
+{
+	//set values in address struct for connection
+	_address.sin_family = dom;
+	_address.sin_port = htons(port);
+	_address.sin_addr.s_addr = htonl(interf);
+	//establish ths ssocket
+	_socket = socket(dom, serv, protoc);
+}
