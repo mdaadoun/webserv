8c2f4cf9cc71e5bdcf439894ffc8c1f295cce0a1
Dlaidet <110157355+Dlaidet@users.noreply.github.com>
2023-02-14 15:56:49 +0100

    start
    Co-authored-by: kamiakze69j <kamiakze69j@gmail.com>
    Co-authored-by: Dylan Laidet <dlaidet@1A7.42angouleme.fr>

---
+++ inc/parsing/parsing.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amorel <amorel@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 15:14:07 by amorel            #+#    #+#             */
+/*   Updated: 2023/02/14 15:38:46 by amorel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_HPP
+#define PARSING_HPP
+
+#include <iostream>
+#include <map>
+#include <string>
+
+
+#endif // PARSING_HPP
