803079ce85cd95d9927720d083d28c665b6edd75
Michaël DAADOUN <97439873+mdaadoun@users.noreply.github.com>
2023-02-17 10:24:05 +0100

    Tester development, config editor (#18)
    * delete requirements
    * adding the menu
    * TODO
    * road to app
    * config editor update

--- tst/config.py
+++ tst/config.py
@@ -9,44 +9,45 @@ def start(config):
         return None
     output = config.split(":")[1]
     conf = input.split(",")
-    data.config["request"]["command"] = conf[0]
-    data.config["request"]["path"] = conf[1]
-    data.config["request"]["host"] = conf[2]
-    data.config["request"]["port"] = conf[3]
-    data.config["request"]["protocol"] = conf[4]
-    return data.config
+    data.request["command"] = conf[0]
+    data.request["path"] = conf[1]
+    data.request["host"] = conf[2]
+    data.request["port"] = conf[3]
+    data.request["protocol"] = conf[4]
+    return data.request
 
 
-# generate a config.ini file for ./webserv <FILE>
+# generate a config_default.ini file for ./webserv <FILE>
 def create_file(content):
-    printr("[green]Generated a config.ini file.[/green]")
+    printr("[green]Generated a config_default.ini file.[/green]")
     printr("File content:")
     print(content)
-    with open("config.ini", "w") as file:
+    with open("conf/config_default.ini", "w") as file:
         file.write(content)
 
 
 def build_content():
-    content = ''
+    content = '[server]\n'
     for c in data.config['server']:
         if c == "error_page":
             for e in data.config['server'][c]:
                 content += c + '_' + e + "=" + data.config['server'][c][e] + '\n'
         elif c == "location":
             for l in data.config['server'][c]:
-                content += c + '_' + l + "=" + 'allow_methods:' + str(data.config['server'][c][l]['allow_methods']) + '\n'
+                content += c + '_' + l + "=" + 'allow_methods:' + str(
+                    data.config['server'][c][l]['allow_methods']) + '\n'
         else:
             content += c + "=" + str(data.config['server'][c]) + '\n'
     return content
 
 
-def generate_file():
+def editor():
     printr("[bold yellow]Config file generator:[/bold yellow]")
     console = Console()
     ans = console.input("""
-Do you want to generate a quick default config.ini file ?
-[bold green]y/yes[/bold green]: generate quick default config.ini file.
-[bold red]n/no[/bold red]: generate config.ini with question and answers.
+Do you want to generate a quick default config_default.ini file ?
+[bold green]y/yes[/bold green]: generate quick default config_default.ini file.
+[bold red]n/no[/bold red]: generate config_default.ini with question and answers.
 
 [green](default=yes)[/green] > """)
     print(ans)
@@ -60,4 +61,4 @@ Do you want to generate a quick default config.ini file ?
 
 
 if __name__ == '__main__':
-    generate_file()
+    editor()
