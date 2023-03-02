dde5cb3f4af84eb4259f4bef8d52175ffde8dde5
Timoca <91198334+Timocaa@users.noreply.github.com>
2023-03-02 11:10:52 +0100

    Feat/request (#45)
    * starting request parsing
    
    * request parsing in progress
    
    * for the reunion
    
    * testing parsing request (just the begin)
    
    * implement parsing request (just the begin)
    
    ---------
    
    Co-authored-by: Thomas Lafont <tlafont@2F4.42angouleme.fr>
    Co-authored-by: Michaël DAADOUN <97439873+mdaadoun@users.noreply.github.com>

--- src/test/main.test.cpp
+++ src/test/main.test.cpp
@@ -6,7 +6,7 @@
 /*   By: amorel <amorel@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 12:47:09 by fleblanc          #+#    #+#             */
-/*   Updated: 2023/02/17 17:52:33 by amorel           ###   ########.fr       */
+/*   Updated: 2023/03/01 12:35:54 by tlafont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 		std::cout << "1 - Socket tests" << std::endl;
         std::cout << "2 - Parsing tests" << std::endl;
         std::cout << "3 - Cgi tests" << std::endl;
-        std::cout << "4 - Handling request tests" << std::endl;
+        std::cout << "4 - parse request tests" << std::endl;
 		std::cout << "9 - All tests" << std::endl;
 
 		std::cout << "Command : ";
@@ -47,12 +47,13 @@ int main(int argc, char **argv)
             cgiTest();
             break;
         case 4:
-            handlerRequestTest();
+            requestTest();
             break;
 		case 9:
 			//socketTest();
 			parsingTest();
             cgiTest();
+			requestTest();
 			responseTest();
 			break;
 		default:
